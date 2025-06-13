a9fc78afce284bc23ebe6de050fb808e6201fe4b
devIncogni <arinkumar10@outlook.com>
2025-06-14 00:19:31 +0530

    Wrote a function to recursively print name

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
-#include "helloworld.h"
 
+#include <iostream>
+#include <string_view>
+void printName(std::string_view name, int times) {
+  if (times <= 0) {
+    return;
+  } else {
+    std::cout << name << '\n';
+    printName(name, times - 1);
+    return;
+  }
+}
 int main() {
-  print("Hello World");
+
+  printName("Arin", 5);
   return 0;
 }
