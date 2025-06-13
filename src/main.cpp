b66632465c53af4de9df30ff831f36897485026d
devIncogni <arinkumar10@outlook.com>
2025-06-14 00:22:25 +0530

    Wrote functions to print forwards and backwards natural numbers upto N

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,29 @@ void printName(std::string_view name, int times) {
     return;
   }
 }
+
+void printUpto(int N) {
+  if (N <= 0) {
+    return;
+  } else {
+    printUpto(N - 1);
+    std::cout << N << '\n';
+  }
+}
+
+void printFrom(int N) {
+  if (N <= 0) {
+    return;
+  } else {
+    std::cout << N << '\n';
+    printFrom(N - 1);
+  }
+}
+
 int main() {
 
   printName("Arin", 5);
+  printUpto(10);
+  printFrom(10);
   return 0;
 }
